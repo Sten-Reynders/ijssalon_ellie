6f5aaf3b6689ac35300c79e07b9cafd66e3d4ce6
Sten Reynders <sten.reynders@gmail.com>
2024-07-07 18:59:53 +0200

    boekhouding en presentatie functie toegevoegd

---
+++ presentatie.py
@@ -0,0 +1,19 @@
+def presenteer (mijn_dict, totaal) :
+    for key, value in mijn_dict.items ():
+        print (f" {key} : {value} euro" )
+
+    print ("==========================")
+    print (f"totaal : {totaal} euro")
+
+'''
+mijn_dict = {
+    'vis' : 10,
+    'vlees' : 25,
+    'overig' : 15
+}
+
+totaal = 50
+
+presenteer (mijn_dict, totaal)
+
+'''
